f97ac94f3ac38a7988e7df095cc54136f45a67ab
maxbrackets <159195145+maxbrackets@users.noreply.github.com>
2024-02-07 21:07:27 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function openWin(link) {
+    let win = window.open();
+    let iframe = win.document.createElement('iframe');
+    win.document.write("<html><head><link href='style.css' rel='stylesheet' type='text/css' /><meta name='viewport' content='width=device-width'></head></html>")
+    iframe.style.width = '100%';
+    iframe.style.height = '100%';
+    iframe.style.border = 'none';
+    iframe.style.margin = 'none';
+    iframe.src = link;
+    win.document.body.style.margin = '0px';
+    win.document.body.appendChild(iframe);
+  }
